cc90905b8b0c448a0ee5c7e7c7381ec583397352
skum005 <santosh.sri52@gmail.com>
2016-04-15 23:16:46 +0530

    adding build.xml and log4j

---
+++ target/cucumber-html-report/report.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("datatable.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Data Table Feature",
+  "description": "In Order to learn data table feature\r\nAs a learner\r\nI want to create an example",
+  "id": "data-table-feature",
+  "keyword": "Feature"
+});
